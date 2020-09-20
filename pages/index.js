ea42bd56941e6aa74a1a40a2dc9f3dfc3f50e770
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 00:27:44 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+class Index extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Sample Shopify App using React and Next.js</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
